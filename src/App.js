9e801dfb6dd386b400926167d22bad5417f55554
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-05-14 11:12:42 -0300

    feat(ui): add semantic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Menu } from 'semantic-ui-react'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu inverted>
+        <Menu.Item header>The Resistance</Menu.Item>
+      </Menu>
     </div>
   );
 }
-
-export default App;
